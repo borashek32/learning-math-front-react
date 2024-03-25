eaeb7719f117886a82a4cfef74019e92dfb3bde5
Nataly Baranova <borashek@inbox.ru>
2024-03-25 11:32:54 +0300

    feat: add pagination

--- src/common/components/pagination/Pagination.tsx
+++ src/common/components/pagination/Pagination.tsx
@@ -2,9 +2,7 @@ import styles from './Pagination.module.sass'
 import { Props } from './Pagination.types'
 import arrowLeft from './../../assets/icons/arrow-left.svg'
 import arrowRight from './../../assets/icons/arrow-right.svg'
-import { Link } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export const Pagination = ({ 
   pages,
@@ -12,25 +10,9 @@ export const Pagination = ({
   prev,
   onPageChange,
 }: Props) => {
-  const location = useLocation()
   const [startPage, setStartPage] = useState<number>(1)
-  const [endPage, setEndPage] = useState<number>(5)
 
-  const pageNumber = parseInt(new URLSearchParams(location.search).get('page') || '1')
-
-  console.log(startPage, endPage)
-
-  const handlePageClick = (pageNumber: number) => {
-    onPageChange(pageNumber)
-    
-    if (pageNumber <= 3) {
-      setStartPage(1)
-      setEndPage(5)
-    } else if (pageNumber >= pages - 2) {
-      setStartPage(pageNumber - 2)
-      setEndPage(pageNumber + 2)
-    } 
-  }
+  console.log(startPage)
 
   const getPageNumberFromUrl = (url: string): number => {
     const pageNumberRegex = /page=(\d+)/
@@ -40,12 +22,22 @@ export const Pagination = ({
     return pageNumber
   }
 
+  const handlePageClick = (pageNumber: number) => {
+    onPageChange(pageNumber)
+    
+    if (pageNumber <= 3) {
+      setStartPage(1)
+    } else if (pageNumber >= 3) {
+      setStartPage(pageNumber)
+    } 
+  }
+
   return (
     <div className={styles.paginationWrapper}>
       <div onClick={() => handlePageClick(getPageNumberFromUrl(prev))} className={styles.arrow}>
         <img src={arrowLeft} alt="arrow left" width="30" />
       </div>
-      {Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index).map((pageNumber) => (
+      {Array.from({ length: 5 }, (_, index) => startPage + index).map((pageNumber) => (
         <div key={pageNumber} className={styles.page}>
           <p className={styles.pageNumber} onClick={() => handlePageClick(pageNumber)}>{pageNumber}</p>
         </div>
