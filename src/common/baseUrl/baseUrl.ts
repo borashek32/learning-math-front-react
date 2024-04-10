06ffd7d2648571688c09f6503521d4c40aede17c
Nataly Baranova <borashek@inbox.ru>
2024-04-10 19:59:09 +0300

    feat: correct baseUrl

--- src/common/baseUrl/baseUrl.ts
+++ src/common/baseUrl/baseUrl.ts
@@ -1,3 +1,3 @@
-export const baseURL = 'http://localhost:7001/api/'
+// export const baseURL = 'http://localhost:7001/api/'
 // export const baseURL = 'https://92d8-185-237-80-206.ngrok-free.app/api/'
-// export const baseURL = 'https://learning-math-back-node.vercel.app/api/'
+export const baseURL = 'https://learning-math-back-node.vercel.app/api/'
