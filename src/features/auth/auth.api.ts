e82a173e438cfe9b10e0b0f7ae77aabb469a2f3a
Nataly Baranova <borashek@inbox.ru>
2024-04-26 17:39:27 +0300

    feat: no cors mode

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -23,6 +23,7 @@ type RefreshDataType = {
 const baseQuery = fetchBaseQuery({
   baseUrl: baseURL,
   method: 'POST',
+  mode: 'no-cors',
   credentials: 'include',
   headers: {
     'Content-Type': 'application/json',
